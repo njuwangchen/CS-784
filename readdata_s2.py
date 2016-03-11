95df4cc7f2058996a1e65ad05abe70760a6bc860
njuwangchen <wangchenclark0512@gmail.com>
2016-03-11 16:11:06 -0600

    readdata_s2

---
+++ readdata_s2.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+__author__ = 'ClarkWong'
+import json
+import pickle
+import random
+import xlwt
+
+product_dict = {}
+product_list = []
+
+f = open('elec_pairs_stage2.txt', 'r')
+
+i = 1
+for line in f:
+    print "reading line", i
+    i = i+1
+    list_line = line.split('?')
+
+    productID = list_line[1]
+    productInfo = json.loads(list_line[2], encoding = 'latin-1')
+    product_dict[productID] = productInfo
+    productName = productInfo[u'Product Name']
+    productTuple = (productID, productName)
+    product_list.append(productTuple)
+
+f.close()
+
+with open('prodID_dict_s2.pickle', 'wb') as handle:
+    pickle.dump(product_dict, handle)
+
+sample_idx = [random.randint(0, 10000) for r in range(350)]
+samples = []
+for r in sample_idx:
+    print product_list[r]
+    samples.append(product_list[r])
+
+workbook = xlwt.Workbook()
+sheet = workbook.add_sheet("Sample1")
+
+row = 0
+for i in samples:
+    sheet.write(row, 0, i[0])
+    sheet.write(row, 1, i[1])
+    row=row+1
+
+workbook.save("Samples.xls")
+
+
+
+
+
+
+
+
