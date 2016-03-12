616e1cf94eddd5667477f9c3b6be67a87d699ee3
Mingyuan Tian <mingyuan@macaroni-04.cs.wisc.edu>
2016-03-11 20:06:27 -0600

    Read brand_dict and put it in brand_dict dictionary. read the Samples_clean.xlsx and then shuffle, and store the ProductName and BrandName into 2 lists

--- extractor1.py
+++ extractor1.py
@@ -2,15 +2,16 @@ from openpyxl import load_workbook
 wb = load_workbook('Samples_clean.xlsx')
 ws = wb.active
 
+# read the golden data from the excel file
 ProductName_sorted=[];
 for cellObj in ws.columns[1]:
         ProductName_sorted.append(str(cellObj.value))
-
 BrandName_true_sorted=[];
 for cellObj in ws.columns[2]:
         BrandName_true_sorted.append(str(cellObj.value))
 # No brand read as "None"
 
+####shuffle
 from random import shuffle
 ProductName = []
 BrandName_true = []
@@ -19,27 +20,25 @@ r=0.5  # shuffle use the same seed every time
 shuffle(index_shuf,lambda: r)
 shuffle(index_shuf,lambda: r)
 shuffle(index_shuf,lambda: r)
-print index_shuf
+#print index_shuf
 for i in index_shuf:
     ProductName.append(ProductName_sorted[i])
     BrandName_true.append(BrandName_true_sorted[i])
-    print str(i)+","+BrandName_true[-1]+"==== "+ProductName[-1]
+    #print str(i)+","+BrandName_true[-1]+"==== "+ProductName[-1]
+#print len(ProductName)
+#print len(BrandName_true)
 
+##### read the brand dictionary, key is the brand name, value is the frequency
+brand_dict ={}
+#f = open('elec_brand_dic.txt', 'r')
+import codecs
+f = codecs.open('elec_brand_dic.txt', 'r', encoding='utf-8')
 
-#print ProductName
-#print BrandName_true
-#print wb['sheet1']
-#print ws.get_sheet_names()
-#cell_range = ws['A1':'C2']
-#print ws['A1'].value
-
-#for i in range(1, 351):
-        #print(i, ws.cell(row=i, column=2).value)
-
-
-#for row in ws.iter_rows():
-#    print row
-
-# check out the last row
-#for cell in row:
-#    print cell
+for line in f:
+        list_line = line.split('\t')
+        brand_dict[list_line[0]]=int(list_line[1])
+        #print list_line
+f.close()
+#print len(brand_dict)
+from pprint import pprint
+#pprint(brand_dict)
