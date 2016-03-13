0d146c7990d757a1f02d2a3b76cf2051e6eb4fac
njuwangchen <wangchenclark0512@gmail.com>
2016-03-12 21:31:25 -0600

    extractor updated

--- extractor1.py
+++ extractor1.py
@@ -1,4 +1,47 @@
 from openpyxl import load_workbook
+from random import shuffle
+import codecs
+
+def predictAndTest(productNamesSet, trueBrand):
+    brand_predicted = []
+    for productName in productNamesSet:
+        list_words_in_productName = productName.split()
+        brandFound = False
+
+        if(not brandFound):
+            for i in range(len(list_words_in_productName)-2):
+                word_two_token = list_words_in_productName[i]+" "+list_words_in_productName[i+1]+" "+list_words_in_productName[i+2]
+                if word_two_token.lower() in brand_dict:
+                    brand_predicted.append(word_two_token)
+                    brandFound = True
+                    break
+        if(not brandFound):
+            for i in range(len(list_words_in_productName)-1):
+                word_two_token = list_words_in_productName[i]+" "+list_words_in_productName[i+1]
+                if word_two_token.lower() in brand_dict:
+                    brand_predicted.append(word_two_token)
+                    brandFound = True
+                    break
+
+        if(not brandFound):
+            for word in list_words_in_productName:
+                if word.lower() in brand_dict:
+                    brand_predicted.append(word)
+                    brandFound = True
+                    break
+
+        if(not brandFound):
+            brand_predicted.append("None")
+
+    count = 0
+    for i in range(len(brand_predicted)):
+        if(brand_predicted[i].lower() == trueBrand[i].lower()):
+            count=count+1
+        else:
+            print "======" + productNamesSet[i]
+            print brand_predicted[i]+","+trueBrand[i]
+    return count
+
 wb = load_workbook('Samples_clean.xlsx')
 ws = wb.active
 
@@ -12,7 +55,6 @@ for cellObj in ws.columns[2]:
 # No brand read as "None"
 
 ####shuffle
-from random import shuffle
 productNames = []
 brandNames_true = []
 
@@ -36,8 +78,6 @@ for i in index_shuf[120:]:
 
 ##### read the brand dictionary, key is the brand name( lower case), value is the frequency
 brand_dict ={}
-#f = open('elec_brand_dic.txt', 'r')
-import codecs
 f = codecs.open('elec_brand_dic.txt', 'r', encoding='utf-8')
 
 for line in f:
@@ -46,56 +86,9 @@ for line in f:
         brand_dict[list_line[0].lower()]=int(list_line[1])
         #print list_line
 f.close()
-#print len(brand_dict)
-from pprint import pprint
-#pprint(brand_dict)
 
-#### tokenize each product name on word level and see if any token is in the dictionary
-brand_predicted = []
-for productName in productNames:
-    list_words_in_productName = productName.split()
-    #print productName
-    #print list_words_in_productName
-    brandFound = False
+correctNumOfTrain = predictAndTest(productNames, brandNames_true)
+print correctNumOfTrain
 
-    if(not brandFound):
-        for i in range(len(list_words_in_productName)-2):
-            word_two_token = list_words_in_productName[i]+" "+list_words_in_productName[i+1]+" "+list_words_in_productName[i+2]
-            if word_two_token.lower() in brand_dict:
-                brand_predicted.append(word_two_token)
-                brandFound = True
-                break
-    if(not brandFound):
-        for i in range(len(list_words_in_productName)-1):
-            word_two_token = list_words_in_productName[i]+" "+list_words_in_productName[i+1]
-            if word_two_token.lower() in brand_dict:
-                brand_predicted.append(word_two_token)
-                brandFound = True
-                break
-
-    if(not brandFound):
-        for word in list_words_in_productName:
-            if word.lower() in brand_dict:
-                brand_predicted.append(word)
-                brandFound = True
-                break
-
-
-    if(not brandFound):
-        brand_predicted.append("None")
-
-
-
-count = 0
-for i in range(0,len(brand_predicted)):
-    if(brand_predicted[i].lower() == brandNames_true[i].lower()):
-        count=count+1
-    else:
-        print "======" +productNames[i]
-        print brand_predicted[i]+","+brandNames_true[i]
-print count
-#print brand_predicted
-#print brandNames_true
-
-# pay attention to brandname with 2 words
-# generate candidate set if multiple matches
+correctNumOfTest = predictAndTest(productNames_test, brandNames_true_test)
+print correctNumOfTest
