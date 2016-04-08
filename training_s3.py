84d4fe79c0ae86f8760b0dbe81658498dba7da23
mtian29 <zactmy@gmail.com>
2016-04-08 04:03:54 -0500

    training to select the best classfier and tuning parameter

--- training_s3.py
+++ training_s3.py
@@ -1,6 +1,7 @@
 import pickle
 from sklearn.metrics import precision_score, recall_score
-from sklearn import svm
+from sklearn import svm, linear_model
+from sklearn.naive_bayes import GaussianNB
 
 with open('classlabels_train.pickle', 'rb') as handle:
 	classlabels_train = pickle.load(handle)
@@ -14,25 +15,45 @@ with open('feature_matrix_test.pickle', 'rb') as handle:
 #print len(classlabels_test), len(classlabels_train),len(feature_matrix_test),len(feature_matrix_train)
 k = 9900   # 9900 out of 10000 as the training set, 10000-9900 as the tuning set
 X = feature_matrix_train[:k]
-y = classlabels_train[:k]
-y_true_svm = classlabels_train[-(10000-k):]
+Y = classlabels_train[:k]
+
+#tuning
+y_true = classlabels_train[-(10000-k):]
+x = feature_matrix_train[-(10000-k):]
 
 ####Decision Tree
 
 ####Random Forest
 
+
+
 ####Naive Bayes
+nb = GaussianNB()
+print nb.fit(X,Y)
+y_pred_nb = nb.predict(x).tolist()
+print "naive bayes"
+print "P; %f" % precision_score(y_true, y_pred_nb)
+print "R; %f" % recall_score(y_true, y_pred_nb)
+#print(metrics.classification_report(y_true, y_pred_nb))
+#print(metrics.confusion_matrix(y_true, y_pred_nb))
+
 
 ####Support Vector Machines
 clf = svm.SVC()
-print clf.fit(X, y)
+print clf.fit(X, Y)
 #my_list[-5:] # grab the last five elements
-y_pred_svm = clf.predict(feature_matrix_train[-(10000-k):]).tolist()
+y_pred_svm = clf.predict(x).tolist()
 #print y_true_svm
 #print y_pred_svm
 print "SVM"
-print "P; %f" % precision_score(y_true_svm, y_pred_svm)
-print "R; %f" % recall_score(y_true_svm, y_pred_svm)
+print "P; %f" % precision_score(y_true, y_pred_svm)
+print "R; %f" % recall_score(y_true, y_pred_svm)
 
 ####Logistic Regression
-
+logreg = linear_model.LogisticRegression(C=1)
+print logreg.fit(X, Y)
+y_pred_logreg = logreg.predict(x).tolist()
+#print y_pred_logreg
+print "Log Reg"
+print "P; %f" % precision_score(y_true, y_pred_logreg)
+print "R; %f" % recall_score(y_true, y_pred_logreg)
